a3a6ebbc8e2f3b98d89a4f9bf8f60d8835a26a38
javierlga <javierlgac@gmail.com>
2020-02-26 18:42:49 -0800

    Updated gitignore to exclude tf files and jenkins installation script used in packer

--- packer/scripts/install-jenkins.sh
+++ packer/scripts/install-jenkins.sh
@@ -10,8 +10,8 @@ utilities() {
 }
 
 jenkins() {
-   wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
-   rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+   sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+   sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
    sudo yum -y update
    sudo yum install jenkins java -y
 }
